23004732b63de2512db7763f3d61add6d350de94
mattmuchnok <mattmuchnok@gmail.com>
2022-02-21 11:55:14 -0500

    inital and final commit

---
+++ buildTower.js
@@ -0,0 +1,26 @@
+function towerBuilder(nFloors) {
+    const tower = []
+    let towerString = '*'
+      for(let i = 0; i<nFloors; i++){ 
+        tower.push(towerString)
+        towerString += "**"
+        for (let j = 0; j<i; j++){
+          tower[j] = " " + tower[j] + ' '
+        }
+      }
+      return tower
+    }
+    
+    //alternatively
+    // function towerBuilder(nFloors) {
+    //   var tower = [];
+    //   for (var i = 0; i < nFloors; i++) {
+    //     tower.push(" ".repeat(nFloors - i - 1)
+    //              + "*".repeat((i * 2)+ 1)
+    //              + " ".repeat(nFloors - i - 1));
+    //   }
+    //   return tower;
+    // }
+    
+
+    console.log(towerBuilder(6))
